66cff6e5c6b4ac78e215b14b5ace8654048f05d9
devans1913 <dwynn.evans@gmail.com>
2018-11-10 15:29:07 -0600

    updating routes

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,34 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with ME
 var friendList = [
-    {
-      Name: "Kira",
-      name: "Kira", 
-      scores: ["5", "3", "2", "1", "5", "5", "1", "3", "3", "1"]
-    },
-    {
-      Name: "Stephon",
-      name: "Stephon",
-      scores: ["2", "4", "1", "4", "2", "5", "1", "4", "1", "3"]
-    },
-    {
-      Name: "Dwynn",
-      name: "Dwynn",
-      scores: ["5", "4", "4", "3", "2", "5", "3", "3", "1", "5"]
-    },
-    {
-      Name: "Chelsia",
-      name: "Chelsia",
-      scores: ["4", "1", "1", "3", "2", "1", "4", "5", "3", "1"]
-    },
-    {
-        Name: "Deondre",
-        name: "Deondre",
-        scores: ["5", "1", "4", "1", "3", "1", "2", "1", "5", "1"]
-    },
-    {
-        Name: "Michael",
-        name: "Michael",
-        scores: ["2", "1", "3", "5", "4", "3", "1", "1", "2", "4"]
-    },
-  ];
-  
-  module.exports = friendList;
+  {
+    name: 'Dwynn Evans',
+    profilePic: 'https://5ho3t84rhq-flywheel.netdna-ssl.com/wp-content/uploads/2018/07/Dwynn-Spotlight-169x300.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,11 +6,11 @@ var path = require('path');
 module.exports = function(app){
   
   app.get('/survey', function (req, res) {
-    res.sendFile(path.join(__dirname + '../public/survey.html'));
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 
-  //route to home page incase path doesn't match
-  app.get("*",function (req, res) {
-    res.sendFile(path.join(__dirname + '../public/home.html'));
+  //a USE route to home page
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
 };
